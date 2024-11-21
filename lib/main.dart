8d2e18e26b7c708288194f79490f0514ba282164
tuannguyen1302 <tuanhienkun13102002@gmail.com>
2024-11-21 15:17:09 +0700

    feat signup with firebase done

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'dart:async';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
+import 'firebase_options.dart';
 import 'presentation/app.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   Future<void> startApp() async {
     WidgetsFlutterBinding.ensureInitialized();
     runApp(
--- lib/presentation/features/sign_up/provider/signup_viewmodel.dart
+++ lib/presentation/features/sign_up/provider/signup_viewmodel.dart
@@ -1,6 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 import '../../../../utilities/popup_messages.dart';
+import '../../../global_loader/global_loader.dart';
 import 'signup_state.dart';
 
 part 'signup_viewmodel.g.dart';
@@ -26,21 +30,24 @@ class SignUpViewModel extends _$SignUpViewModel {
     state = state.copyWith(rePassword: rePassword);
   }
 
-  Future<void> handleSignUp() async {
-    final useName = state.userName;
+  Future<void> handleSignUp(BuildContext _context) async {
+    final userName = state.userName;
     final email = state.email;
     final password = state.password;
     final rePassword = state.rePassword;
 
-    final regex = RegExp(
+    // Regex kiểm tra định dạng email
+    final emailRegex = RegExp(
       r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$',
     );
-    final regex1 = RegExp(
+
+    // Regex kiểm tra định dạng mật khẩu (ít nhất 8 ký tự, bao gồm chữ, số và ký tự đặc biệt)
+    final passwordRegex = RegExp(
       r'^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$',
     );
 
-    // Kiểm tra các trường không được để trống
-    if (useName.isEmpty || state.userName.isEmpty) {
+    // 1. Kiểm tra các trường không được để trống
+    if (userName.isEmpty) {
       await toastInfo('Tên người dùng không được để trống');
       return;
     }
@@ -52,28 +59,51 @@ class SignUpViewModel extends _$SignUpViewModel {
       await toastInfo('Mật khẩu không được để trống');
       return;
     }
-
     if (rePassword.isEmpty) {
       await toastInfo('Xác nhận mật khẩu không được để trống');
       return;
     }
 
-    // Kiểm tra định dạng email
-    if (!regex.hasMatch(email)) {
+    // 2. Kiểm tra định dạng email
+    if (!emailRegex.hasMatch(email)) {
       await toastInfo('Email không hợp lệ');
       return;
     }
 
-    // Kiểm tra định dạng mật khẩu
-    if (!regex1.hasMatch(password)) {
-      await toastInfo('Mật khẩu không hợp lệ');
+    // 3. Kiểm tra định dạng mật khẩu
+    if (!passwordRegex.hasMatch(password)) {
+      await toastInfo(
+        'Mật khẩu không hợp lệ. Mật khẩu cần ít nhất 8 ký tự, bao gồm chữ, số và ký tự đặc biệt',
+      );
       return;
     }
 
-    // Kiểm tra mật khẩu và xác nhận
-    if (state.password != state.rePassword || password != rePassword) {
-      await toastInfo('Mật khẩu không trùng khớp');
+    // 4. Kiểm tra mật khẩu và xác nhận mật khẩu
+    if (password != rePassword) {
+      await toastInfo('Mật khẩu và xác nhận mật khẩu không trùng khớp');
       return;
     }
+    ref.read(globalLoaderProvider.notifier).setLoaderValue(true);
+    Future.delayed(
+      const Duration(seconds: 2),
+      () async {
+        try {
+          final credential = await FirebaseAuth.instance
+              .createUserWithEmailAndPassword(email: email, password: password);
+          if (credential.user != null) {
+            await credential.user?.sendEmailVerification();
+            await credential.user?.updateDisplayName(userName);
+            print('=============${credential.user}');
+            await toastInfo('dang ki thanh cong ');
+            Navigator.of(_context).pop();
+          }
+        } catch (e) {
+          if (kDebugMode) {
+            print(e.toString());
+          }
+        }
+        ref.read(globalLoaderProvider.notifier).setLoaderValue(false);
+      },
+    );
   }
 }
