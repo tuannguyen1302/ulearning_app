127e7277852851ad68784d3cf51c5f8d4e972541
tuannguyen1302 <tuanhienkun13102002@gmail.com>
2025-03-13 22:21:10 +0700

    done

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,13 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'firebase_options.dart';
 import 'presentation/app.dart';
 
 Future<void> main() async {
   Future<void> startApp() async {
     WidgetsFlutterBinding.ensureInitialized();
     await Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform,
+    //  options: DefaultFirebaseOptions.currentPlatform,
     );
     runApp(
       const ProviderScope(
